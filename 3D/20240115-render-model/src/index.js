c1e171311b5040e6db502bf60b5101cb2f2797a6
danica <xxxleoninexxx@gmail.com>
2024-01-18 11:02:43 +0800

    add .glb model source，add draco copy

--- 3D/20240115-render-model/src/index.js
+++ 3D/20240115-render-model/src/index.js
@@ -1,6 +1,10 @@
 import * as THREE from 'three'
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
 import ringModel from '@/assets/alliance_white_gold_ring.obj'
+import ringCluster from '@/assets/ring_cluster.glb'
+import cameraCanon from '@/assets/camera_canon.glb'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.0001, 1000)
@@ -8,45 +12,49 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-// function renderCubeDemo () {
-//   const geometry = new THREE.BoxGeometry(1, 1, 1)
-//   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-//   const cube = new THREE.Mesh(geometry, material)
-//   scene.add(cube)
-//   console.log(cube)
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
-//   camera.position.z = 5
+function renderCubeDemo () {
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+  console.log(cube)
 
-//   function animate () {
-//     requestAnimationFrame(animate)
-//     cube.rotation.x += 0.01
-//     cube.rotation.y += 0.01
-//     // cube.rotation.x += 0.01;
-//     renderer.render(scene, camera)
-//   }
-//   animate()
-// }
+  camera.position.z = 5
+
+  function animate () {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    // cube.rotation.x += 0.01;
+    renderer.render(scene, camera)
+  }
+  animate()
+}
 // renderCubeDemo()
 
 function renderRing () {
-  let ring = undefined
   const loader = new OBJLoader()
   loader.load(
     ringModel,
     (object) => {
-      console.log(object)
       scene.add(object)
-
       camera.position.z = 3
-      
 
       // 添加环境光
       const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
       scene.add(ambientLight)
 
+      // 添加点光源
       const pointLight = new THREE.PointLight(0xffffff, 500)
       pointLight.position.set(0, 0, 0)
       scene.add(pointLight)
+
       // 启用阴影
       renderer.shadowMap.enabled = true
 
@@ -56,26 +64,74 @@ function renderRing () {
       function animate () {
         requestAnimationFrame(animate)
         object.rotation.y += 0.01
-      
-        // object.rotation.y += 0.01
-        // cube.rotation.x += 0.01;
         renderer.render(scene, camera)
       }
       animate()
-    },
-    function (xhr) {
-      console.log("onProgress", xhr)
-    },
-    function (error) {
-      console.log("onError", error)
     }
   )
-
 }
-renderRing()
+// renderRing()
 
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
+
+function renderRingCluster () {
+  const dracoLoader = new DRACOLoader()
+  dracoLoader.setDecoderPath('/assets/draco/gltf/')
+  const loader = new GLTFLoader()
+  loader.setDRACOLoader(dracoLoader)
+  loader.load(
+    ringCluster,
+    (object) => {
+      const model = object.scene
+      scene.add(model)
+      camera.position.z = 3
+
+      // 添加环境光
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+      scene.add(ambientLight)
+
+      renderer.shadowMap.enabled = true
+
+      // 为模型和光源设置阴影
+      model.castShadow = true
+
+      function animate () {
+        requestAnimationFrame(animate)
+        model.rotation.z += 0.001
+        renderer.render(scene, camera)
+      }
+      animate()
+    }
+  )
+}
+// renderRingCluster()
+
+function renderCanon () {
+  const dracoLoader = new DRACOLoader()
+  dracoLoader.setDecoderPath('/assets/draco/gltf/')
+  const loader = new GLTFLoader()
+  loader.setDRACOLoader(dracoLoader)
+  loader.load(
+    cameraCanon,
+    (object) => {
+      const model = object.scene
+      scene.add(model)
+
+      //设置相机距离物体的位置
+      camera.position.z = 3
+      // 添加环境光
+      const ambientLight = new THREE.AmbientLight(0xffffff, 100)
+      scene.add(ambientLight)
+
+      function animate () {
+        requestAnimationFrame(animate)
+        model.rotation.z += 0.001
+        model.rotation.y += 0.001
+        model.rotation.x += 0.001
+        renderer.render(scene, camera)
+      }
+      animate()
+    }
+  )
+}
+renderCanon()
+
--- 3D/20240115-render-model/webpack.config.js
+++ 3D/20240115-render-model/webpack.config.js
@@ -1,18 +1,29 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   mode: 'none',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    wasmLoading: 'async-node',
   },
   plugins: [
     new HTMLWebpackPlugin({
       title: "3D render Practice"
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `${path.dirname(
+            require.resolve(`three/examples/jsm/libs/draco/gltf/draco_decoder.js`)
+          )}`,
+          to: 'assets/draco/gltf'
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
@@ -22,16 +33,22 @@ module.exports = {
         generator: {
           filename: "assets/models/[name].[contenthash:6][ext]"
         }
-      }
+      },
+      {
+        test: /\.glb/i,
+        use: 'file-loader'
+      },
     ],
   },
   devServer: {
     static: './dist'
   },
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       '@': path.resolve(__dirname, 'src')
     }
-    
+  },
+  experiments: {
+    asyncWebAssembly: true,
   }
 }
