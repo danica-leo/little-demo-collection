89c47c61d7ee321465ddadd7527594d481501545
danica <xxxleoninexxx@gmail.com>
2024-03-21 11:40:27 +0800

    continue 5

--- swiftPractice/MyPlayground.playground/Contents.swift
+++ swiftPractice/MyPlayground.playground/Contents.swift
@@ -333,4 +333,88 @@ class Square: NamedShape {
         return "A square with sides of length \(sideLength)"
     }
 }
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
 
+// 5.6.1 Experiment : implement "Circle" class
+class Circle: NamedShape {
+    var radius: Double
+    init(radius:Double,name:String){
+        self.radius = radius
+        super.init(name:name)
+        numberOfSides = 1
+    }
+    
+    func area()->Double{
+        return Double.pi * pow(radius, 2)
+    }
+    
+    override func simpleDescription()->String{
+        return "A circle with radius is \(radius)"
+    }
+}
+let testCircle = Circle(radius:3,name:"my test circle")
+testCircle.area()
+testCircle.simpleDescription()
+
+// 5.7 Getter & Setter
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength:Double,name:String){
+        self.sideLength = sideLength
+        super.init(name:name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength) \\(\\)"
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter=9.9
+print(triangle.sideLength) //双浮点数精度值的问题
+
+// 5.8 willSet & didSet (which means before setting and after setting)
+// TODO: repeat it~
+// WHY:document said "If I don't need compute the property"
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            print("I'm triangle willSet! \(newValue.name)")
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            print("I'm square willSet! \(newValue.name)")
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size:Double,name:String){
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size:10,name:"another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
+
+// 5.9
+// TODO
