478b29d6966e4a47ce73c22d41f5a6ccbb41e98a
danica <xxxleoninexxx@gmail.com>
2024-04-03 11:42:23 +0800

    update very little codes but I am so good!

--- swiftPractice/MyPlayground.playground/Pages/Guide-01-TheBasics.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-01-TheBasics.xcplaygroundpage/Contents.swift
@@ -11,3 +11,14 @@ if environment == "development"{
 }else{
     maximumNumberOfLoginAttemps1 = 10
 }
+
+
+// 1.3 declare multiple constants or multiple variables on a single line
+var x = 0.0, y = 0.0, z = 0.0
+
+// 2.1 Type annotations
+var welcomeMessage: String
+
+// 2.2 assign type value to variable
+welcomeMessage = "Hello"
+
