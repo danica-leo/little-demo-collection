931ee154b08927a30362156f3965d16760d37dbf
danica <xxxleoninexxx@gmail.com>
2024-04-15 11:50:57 +0800

    finish swift Guide - Chapter1  The Basics - section 13 Integer Conversion

--- swiftPractice/MyPlayground.playground/Pages/Guide-01-TheBasics-Sction11-20.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-01-TheBasics-Sction11-20.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 /**
- 11 Numeric Literals
+ 11     Numeric Literals
  */
 
 /* 11.1 integer literal
@@ -28,3 +28,29 @@ let hexadecimalDouble = 0xC.3p0 //真的好像乱码
 let paddedDouble = 000123.456
 let oneMillion  = 1_000_000
 let justOverOneMillon = 1_000_000.000_000_1
+
+/**
+ 12     Numberic Type Conversion
+ DONNOT HAVE DEMOS
+ 
+    when use Int
+    when use other integer types
+ */
+
+/**
+ 13     Integer Conversion
+ */
+
+/*
+ 13.1 A number that overflows will report an error.
+ */
+//let connotBeNegetive: UInt8 = -1
+//let tooBig: Int8 = Int8.max + 1
+
+/*
+ 13.2 Integer Conversion:Convert one specific number type to another
+ */
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
