8a638ee53fc766fbc4de843032867d8f845852c5
danica <xxxleoninexxx@gmail.com>
2024-04-16 11:20:54 +0800

    finish swift Guide - Chapter1  The Basics - section 13 Integer Conversion

--- swiftPractice/MyPlayground.playground/Pages/Guide-01-TheBasics-Sction11-20.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-01-TheBasics-Sction11-20.xcplaygroundpage/Contents.swift
@@ -54,3 +54,55 @@ let twoThousand: UInt16 = 2_000
 let one: UInt8 = 1
 let twoThousandAndOne = twoThousand + UInt16(one)
 
+/**
+ 14     Integer and Floating-Point Conversion
+ */
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+let integerPi = Int(pi)
+
+
+/**
+ 15     Type Aliases
+*/
+typealias AudioSample = UInt16
+var maxAmplitudeFOund = AudioSample.min
+
+/**
+ 16     Booleans
+ */
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    print("Mmm, tasty tunips!")
+} else {
+    print("Eww, turnips are horrible.")
+}
+
+let i = 1
+//if i {
+//    //this example will not compile, and will report an error
+//}
+if i == 1 {
+    // this example will compile successfully
+}
+
+
+/**
+ 17     Tuples
+ */
+let http404Error = (404, "Not Found")
+let (statusCode,statusMessage) = http404Error
+print("code: \(statusCode),message:  \(statusMessage)")
+
+let (justTheStatusCode,_) = http404Error
+print("The status code is \(justTheStatusCode)")
+
+print("code: \(http404Error.0),message:  \(http404Error.1)")
+
+let http200Status = (statusCode:200, description: "OK")
+
+print("code: \(http200Status.0),message:  \(http200Status.1)")
+print("code: \(http200Status.statusCode),message:  \(http200Status.description)")
