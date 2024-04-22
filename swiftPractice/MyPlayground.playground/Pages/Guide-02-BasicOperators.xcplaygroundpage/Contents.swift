1b5a1e652501e5b861a59a3aa9f7261cad5a9152
danica <xxxleoninexxx@gmail.com>
2024-04-22 11:39:41 +0800

    finish swift Guide - Chapter2  Basic Operators - Finished section 4.1

---
+++ swiftPractice/MyPlayground.playground/Pages/Guide-02-BasicOperators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+/*
+ 1  Basic Operators
+ */
+
+/*
+ 2  Terminology
+ */
+
+/*
+ 3 Assignment Operator
+ */
+
+let b = 10
+var a = 5
+a = b
+
+let (x, y) = (1, 2)
+
+if x = y {
+    print("test? Hahahah")
+}
+
+/*
+ 4 Arithmetic Operators
+ 
+ Addtion, Subtraction, Multiplication, Division
+ */
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+
+print("Hello, "+"world!")
+
+
+/**
+  4.1   Remainder Operator
+ */
+
+9 % 4
+
+-9 % 4
