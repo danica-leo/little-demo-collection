b4b79f35d94004ce3966c4302592f8bee6e218ed
danica <xxxleoninexxx@gmail.com>
2024-05-08 11:45:38 +0800

    finish swift Guide - Chapter 03  Strings and Characters - 1.1 Multiline String literals

---
+++ swiftPractice/MyPlayground.playground/Pages/Guide-03-StringsAndCharactors.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+// 0 Strings and Characters
+
+// 1 String Literals
+// 1.1 Multiline String literals
+
+let someString = "Some string literal value"
+
+let quotation = """
+The White Rabbit put on his spectacles.
+"Where shall I begin? please your Majesty?" he said.
+
+"Begin at the beginning,"
+the King said gravely,
+"and go on till you come to the end; then stop."
+"""
