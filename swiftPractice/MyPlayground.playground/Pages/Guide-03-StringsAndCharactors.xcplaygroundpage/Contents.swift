a09c5e147462380d929d49d0f4ccf793c8db174c
danica <xxxleoninexxx@gmail.com>
2024-05-21 14:53:52 +0800

    Swift Guide - Chapter 03  Strings and Characters - 13  Comparing Strings

--- swiftPractice/MyPlayground.playground/Pages/Guide-03-StringsAndCharactors.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-03-StringsAndCharactors.xcplaygroundpage/Contents.swift
@@ -231,3 +231,65 @@ let index12 = greeting12.firstIndex(of:",") ?? greeting12.endIndex
 let beginning = greeting12[..<index]
 
 let newString = String(beginning)
+
+/*
+ 13 Comparing Strings
+ */
+
+/**
+  13.1  String and Character Equality
+ */
+let eAcuteQuestion = "Voules-vous un caf\u{E9}?"
+
+let combinedEAcuteQuestion = "Voules-vous un caf\u{65}\u{301}?"
+
+if eAcuteQuestion == combinedEAcuteQuestion {
+    print("These two strings are considered equal")
+}
+
+let latinCapitalLetterA: Character = "\u{41}"
+
+let cyrillicCapitalLetterA: Character = "\u{0410}"
+
+if latinCapitalLetterA != cyrillicCapitalLetterA {
+    print("These two characters aren't equivalent.")
+}
+
+/**
+  13.2  Prefix and Suffix Equality
+ */
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 "){
+        act1SceneCount += 1
+    }
+}
+print("There are \(act1SceneCount) scene in Act 1")
+
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion")
+    {
+        mansionCount += 1
+    } 
+    else if scene.hasSuffix("Friar Lawrence's cell")
+    {
+        cellCount += 1
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
