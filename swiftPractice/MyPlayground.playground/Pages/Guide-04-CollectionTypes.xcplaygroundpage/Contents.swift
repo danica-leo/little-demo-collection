ff73e08965b211692561d495f2aefdf043b25f39
danica <xxxleoninexxx@gmail.com>
2024-05-23 12:00:40 +0800

    Swift Guide - Chapter 04 Collection Types - 3.2 Arrays - Creating an Empty Array

---
+++ swiftPractice/MyPlayground.playground/Pages/Guide-04-CollectionTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*
+ 1 Collection Types
+ */
+
+/*
+ 2 Mutability of Collections
+ */
+
+/*
+ 3 Arrays
+ */
+
+/**
+ 3.1 Array Type Shorthand Syntax
+ */
+
+
+/**
+ 3.2 Creating an Empty Array
+ */
+
+var someInts:[Int] = []
+print("someInts is of type [Int] with \(someInt.count) items.")
