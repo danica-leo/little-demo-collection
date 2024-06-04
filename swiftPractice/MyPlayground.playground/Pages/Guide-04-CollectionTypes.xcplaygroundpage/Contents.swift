854c78299c9fa3850b17c0e8a995fbf1b7b6ca48
danica <xxxleoninexxx@gmail.com>
2024-06-04 11:54:50 +0800

    Swift Guide - Chapter 04 Collection Types - 5.2  Sets Operations - Set Membership and Equality

--- swiftPractice/MyPlayground.playground/Pages/Guide-04-CollectionTypes.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-04-CollectionTypes.xcplaygroundpage/Contents.swift
@@ -161,3 +161,11 @@ let evenDigits: Set = [0,2,4,6,8]
 let singleDigitPrimeNumbers: Set = [2,3,5,7]
 
 oddDigits.union(evenDigits).sorted()
+oddDigits.intersection(evenDigits).sorted()
+oddDigits.subtracting(singleDigitPrimeNumbers).sorted()
+oddDigits.symmetricDifference(singleDigitPrimeNumbers).sorted()
+
+/**
+ 5.2 Set Memebership and Equality
+ */
+
