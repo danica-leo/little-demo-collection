b9b6e07d97ca9d22131d3df44b01d709017e7432
danica <xxxleoninexxx@gmail.com>
2024-06-07 15:48:18 +0800

    Swift Guide - Chapter 04 Collection Types - Finished ！🎉🎉🎉

--- swiftPractice/MyPlayground.playground/Pages/Guide-04-CollectionTypes.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-04-CollectionTypes.xcplaygroundpage/Contents.swift
@@ -203,6 +203,49 @@ print("The airports dictionary contains \(airports.count) items.")
 
 if airports.isEmpty {
     print("The airports dictionary is empty.")
-}else {
+} else {
     print("The airports dictionary isn't empty.")
 }
+
+airports["LHR"] = "London"
+
+airports["LHR"] = "London Heathrow"
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue)")
+}
+
+if let airportName = airports["DUB"] {
+    print("The name of the airports is \(airportName)")
+} else {
+    print("That airport isn't in the airports dictionary.")
+}
+
+airports["APL"] = "Apple International"
+airports["APL"] = nil
+
+if let removedValue = airports.removeValue(forKey: "DUB"){
+    print("The removed airport's name is \(removedValue).")
+}else{
+    print("The airports dictionary doesn't contain a value for DUB.")
+}
+
+/**
+ 6.5 Iterating Over a Dictionary
+ */
+
+for (airportCode, airportName) in airports {
+    print("\(airportCode):\(airportName)")
+}
+airports.count
+
+for airportCode in airports.keys{
+    print("Airport code:\(airportCode)")
+}
+
+for airportName in airports.values{
+    print("Airport name:\(airportName)")
+}
+
+let airportCodes = [String](airports.keys)
+let airportNames = [String](airports.values)
