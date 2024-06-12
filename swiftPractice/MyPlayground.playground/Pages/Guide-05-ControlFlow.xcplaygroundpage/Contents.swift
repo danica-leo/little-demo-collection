0cbab328d983aad6b75b93c5b44786df53153b17
danica <xxxleoninexxx@gmail.com>
2024-06-12 13:52:10 +0800

    Swift Guide - Chapter 05 Control Flow - 2 For-In Loops

---
+++ swiftPractice/MyPlayground.playground/Pages/Guide-05-ControlFlow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*
+ 1 Control flow
+ */
+
+/*
+ 2 For-In Loops
+ */
+let names = ["Anna","Alex","Brian","Jack"]
+for name in names {
+    print("Hello,\(name)!")
+}
+
+let numberOfLegs = ["spider":8,"ant":6,"cat":4]
+for (animalName,legCount) in  numberOfLegs {
+    print("\(animalName)s have \(legCount) legs")
+}
+
+/*
+ 3?
+ */
