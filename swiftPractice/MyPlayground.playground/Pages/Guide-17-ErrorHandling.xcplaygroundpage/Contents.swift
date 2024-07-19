020e48c0ca9e720dcaf372a29a4c92cd781d9417
danica <xxxleoninexxx@gmail.com>
2024-07-19 14:24:51 +0800

    Swift Guide - Chapter 17 Error Handling - 3.1 Propagating Errors Using Throwing Functions

--- swiftPractice/MyPlayground.playground/Pages/Guide-17-ErrorHandling.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-17-ErrorHandling.xcplaygroundpage/Contents.swift
@@ -7,10 +7,50 @@
  */
 enum VendingMachineError:Error{
     case invalidSelection
-    case insufficientFUnds(coinsNeeded:Int)
+    case insufficientFunds(coinsNeeded:Int)
     case outOfStock
 }
 
-print("test!")
-throw VendingMachineError.insufficientFUnds(coinsNeeded: 5)
+//print("test!")
+//throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
 
+/*
+ 3 Handling Errors
+ */
+
+// 3.1 Propagating Errors Using Throwing Functions
+struct Item {
+    var price:Int
+    var count:Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price:12, count:7),
+        "Chips": Item(price:10, count:4),
+        "Pretzel":Item(price:7, count:11)
+    ]
+    var coinsDeposited=0
+    
+    func vend(itemNamed name:String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+        
+        guard item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+        
+        guard item.price <= coinsDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+        }
+        
+        coinsDeposited -= item.price
+        
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        
+        print("Dispensing \(name)")
+    }
+}
