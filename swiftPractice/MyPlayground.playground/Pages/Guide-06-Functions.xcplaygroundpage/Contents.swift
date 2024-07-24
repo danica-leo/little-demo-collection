cff9d4965d9ea22327e566f0bc585e3a3d1789ac
danica <xxxleoninexxx@gmail.com>
2024-07-24 11:37:51 +0800

    Swift Guide - Chapter 06  Functions - 2.1  Function Parameters and Return Values

--- swiftPractice/MyPlayground.playground/Pages/Guide-06-Functions.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-06-Functions.xcplaygroundpage/Contents.swift
@@ -10,3 +10,17 @@ func greet(person:String) -> String {
     let greeting = "Hello, " + person + "!"
     return greeting
 }
+
+print(greet(person:"Anna"))
+print(greet(person:"Brian"))
+
+func greetAgain(person:String) -> String {
+    return "Hell0, \(person)!"
+}
+
+print(greetAgain(person:"Danica"))
+
+/**
+ 2.1 Function Parameters and Return Values
+ */
+
