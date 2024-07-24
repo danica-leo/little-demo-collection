a26e5607fbb87a65d64b8efd668cda1e58630972
danica <xxxleoninexxx@gmail.com>
2024-07-24 14:24:19 +0800

    Swift Guide - Chapter 06  Functions - 2.4  Functions without return values

--- swiftPractice/MyPlayground.playground/Pages/Guide-06-Functions.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-06-Functions.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@ print(greet(person:"Anna"))
 print(greet(person:"Brian"))
 
 func greetAgain(person:String) -> String {
-    return "Hell0, \(person)!"
+    return "Hello again, \(person)!"
 }
 
 print(greetAgain(person:"Danica"))
@@ -24,3 +24,51 @@ print(greetAgain(person:"Danica"))
  2.1 Function Parameters and Return Values
  */
 
+/**
+ 2.2 Functions With Out Parameters
+ */
+func sayHelloWorld() -> String {
+    return "Hello world!"
+}
+
+print(sayHelloWorld())
+
+/**
+ 2.3 Functions with  multiple parameters
+ */
+func greet(person:String,alreadyGreeted:Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+    
+}
+print(greet(person: "David",alreadyGreeted:true))
+
+
+/**
+ 2.4 Functions without return values
+ */
+
+func greet24(person:String){
+    print("Hooooh, hello \(person)!")
+}
+//let result24 = greet24(person: "Dave")
+//print("result24: \(result24)")
+
+func printAndCount(str:String) -> Int {
+    print(str)
+    return str.count
+}
+
+func printWithoutCounting(str2:String) -> Int{
+    let _ = printAndCount(str:str2)
+}
+/**
+ 2.5 Functions with multiple parameters
+ */
+
+/**
+ 2.6 Functions with an implicit return
+ */
