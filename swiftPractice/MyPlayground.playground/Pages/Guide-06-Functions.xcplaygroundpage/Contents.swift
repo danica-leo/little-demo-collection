dd2b6380dac4ed16035582809f641858f3a43228
danica <xxxleoninexxx@gmail.com>
2024-07-29 18:06:08 +0800

    Swift Guide - Chapter 06  Functions - 5.3 Function Types as Return Types

--- swiftPractice/MyPlayground.playground/Pages/Guide-06-Functions.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-06-Functions.xcplaygroundpage/Contents.swift
@@ -154,3 +154,53 @@ print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
 /*
  5 Function Types
  */
+
+func addTwoInts(_ a:Int , _ b:Int ) -> Int {
+    return a + b
+}
+
+func multiplyTwoInts(_ a:Int, _ b:Int) -> Int {
+    return a * b
+}
+
+func printHelloWorld(){
+    print("hello, world!")
+}
+
+// 5.1 Using Function Types
+var mathFunction:(Int, Int) -> Int = addTwoInts
+
+print("Result: \(mathFunction(2,3))")
+
+let anotherMathFunction = addTwoInts
+
+// 5.2 Function Types as Parameter Types
+func printMathResult(_ mathFunction:(Int, Int) -> Int , _ a:Int, _ b:Int) {
+    print("Result: \(mathFunction(a,b))")
+}
+
+printMathResult(addTwoInts, 3, 5)
+
+// 5.3 Function Types as Return Types
+
+func stepForward(_ input:Int) -> Int {
+    return input + 1
+}
+
+func stepBackward(_ input:Int) -> Int {
+    return input - 1
+}
+
+func chooseStepFunction(backward:Bool) -> (Int) -> Int {
+    return backward ? stepBackward : stepForward
+}
+
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+
+print("Counting to zero:")
+while currentValue != 0 {
+    print("\(currentValue)...")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
