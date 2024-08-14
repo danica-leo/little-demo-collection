f1dfcf6c02fae8d17e5bc8c9faa262ceff2b8822
danica <xxxleoninexxx@gmail.com>
2024-08-14 19:05:08 +0800

    Swift Guide - Chapter 07 Closure - 2.2 Closure Expressiong Syntax

--- swiftPractice/MyPlayground.playground/Pages/Guide-07-Closures.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-07-Closures.xcplaygroundpage/Contents.swift
@@ -13,3 +13,14 @@
  */
 
 let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward(_ s1:String, _ s2:String) -> Bool {
+    return s1 > s2
+}
+var reversedNames = names.sorted(by:backward)
+
+/**
+ 2.2 Closure Expreesiong Syntax
+ */
+reversedNames = names.sorted(by:{(_ s1:String,_ s2:String) -> Bool in return s1 > s2 })
+
