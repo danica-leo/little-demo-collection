b78e7fc2ac64aeddf1b07dbe9128b9a3759547b8
danica <xxxleoninexxx@gmail.com>
2024-08-16 18:55:12 +0800

    Swift Guide - Chapter 07 Closure - 3 Trailing Closures

--- swiftPractice/MyPlayground.playground/Pages/Guide-07-Closures.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-07-Closures.xcplaygroundpage/Contents.swift
@@ -49,3 +49,27 @@ reversedNames = names.sorted(by:{ $0 > $1 })
 */
 
 reversedNames = names.sorted(by: >)
+
+
+/*
+ 3 Trailing Closures
+ */
+reversedNames = names.sorted() { $0 > $1 }
+
+reversedNames = names.sorted { $0 > $1 }
+
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+let numbers = [16,58,510]
+let strings = numbers.map {
+    (number) -> String in
+    var number = number
+    var output = ""
+    repeat {
+        output = digitNames[number % 10]! + output
+        number /= 10
+    } while number > 0
+    return output
+}
