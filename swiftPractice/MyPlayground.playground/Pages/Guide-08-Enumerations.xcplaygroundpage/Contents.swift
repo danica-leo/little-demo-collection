8ae10c53b73d3c29518f16b34b2266ff2474d6af
danica <xxxleoninexxx@gmail.com>
2024-10-17 12:00:02 +0800

    Swift Guide - Chapter 08 Enumerations - Finished! 🎉🎉🎉

--- swiftPractice/MyPlayground.playground/Pages/Guide-08-Enumerations.xcplaygroundpage/Contents.swift
+++ swiftPractice/MyPlayground.playground/Pages/Guide-08-Enumerations.xcplaygroundpage/Contents.swift
@@ -139,3 +139,16 @@ let five = ArithmeticExpression.number(5)
 let four = ArithmeticExpression.number(4)
 let sum = ArithmeticExpression.addition(four, five)
 let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression:ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left,right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) + evaluate(right)
+    }
+}
+
+print(evaluate(product))
